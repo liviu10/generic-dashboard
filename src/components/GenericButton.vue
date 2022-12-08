1c8eca1d858fe337a059ecb89345d24e537cc31d
liviuvoica <87973797+liviuvoica@users.noreply.github.com>
2022-12-08 18:22:17 +0200

    WIP: configured generic button component

---
+++ src/components/GenericButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-btn
+    :label="buttonLabel"
+    :icon="buttonIcon"
+    :no-caps="buttonNoCaps"
+    :no-wrap="buttonNoWrap"
+    :stack="buttonStack"
+    :type="buttonType"
+  />
+</template>
+
+<script setup lang="ts">
+export interface ButtonProps {
+  buttonLabel: string | number;
+  buttonIcon: string;
+  buttonNoCaps: boolean;
+  buttonNoWrap: boolean;
+  buttonStack: boolean;
+  buttonType: string;
+}
+withDefaults(defineProps<ButtonProps>(), {});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,17 @@
 <template>
-  <q-page class="row items-center justify-evenly"> HOME PAGE </q-page>
+  <q-page class="row items-center justify-evenly">
+    <GenericButton
+      buttonLabel="Generic button"
+      buttonIcon="done"
+      :buttonNoCaps="false"
+      :buttonNoWrap="true"
+      :buttonStack="false"
+      buttonType="button"
+    >
+    </GenericButton>
+  </q-page>
 </template>
 
 <script setup lang="ts">
-//
+import GenericButton from 'src/components/GenericButton.vue';
 </script>
