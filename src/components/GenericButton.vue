4a4059fe6344436e13ac8fd95cf0c9c0f129ca98
liviuvoica <87973797+liviuvoica@users.noreply.github.com>
2022-12-09 12:08:03 +0200

    Configured generic button

--- src/components/GenericButton.vue
+++ src/components/GenericButton.vue
@@ -6,17 +6,52 @@
     :no-wrap="buttonNoWrap"
     :stack="buttonStack"
     :type="buttonType"
+    :disable="buttonDisable"
+    :size="buttonSize"
+    :outline="buttonOutline"
+    :flat="buttonFlat"
+    :rounded="buttonRounded"
+    :square="buttonSquare"
+    :padding="buttonPadding"
+    :color="buttonColor"
+    :text-color="buttonTextColor"
+    :dense="buttonDense"
+    :round="buttonRound"
   />
 </template>
 
 <script setup lang="ts">
-export interface ButtonProps {
-  buttonLabel: string | number;
-  buttonIcon: string;
-  buttonNoCaps: boolean;
-  buttonNoWrap: boolean;
-  buttonStack: boolean;
-  buttonType: string;
+interface ButtonProps {
+  buttonLabel?: string | number;
+  buttonIcon?: string;
+  buttonNoCaps?: boolean;
+  buttonNoWrap?: boolean;
+  buttonStack?: boolean;
+  buttonType?: string;
+  buttonDisable?: boolean;
+  buttonSize?: string;
+  buttonOutline?: boolean;
+  buttonFlat?: boolean;
+  buttonRounded?: boolean;
+  buttonSquare?: boolean;
+  buttonPadding?: string;
+  buttonColor?: string;
+  buttonTextColor?: string;
+  buttonDense?: boolean;
+  buttonRound?: boolean;
 }
-withDefaults(defineProps<ButtonProps>(), {});
+withDefaults(defineProps<ButtonProps>(), {
+  buttonLabel: 'Standard button',
+});
 </script>
+
+<style lang="scss" scoped>
+.q-btn {
+  display: unset;
+  position: unset;
+  vertical-align: unset;
+  font-size: 16px !important;
+  line-height: normal;
+  font-weight: 400;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,23 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <GenericButton
-      buttonLabel="Generic button"
-      buttonIcon="done"
-      :buttonNoCaps="false"
+      :buttonLabel="'Generic button'"
+      :buttonIcon="'done'"
+      :buttonNoCaps="true"
       :buttonNoWrap="true"
       :buttonStack="false"
-      buttonType="button"
+      :buttonType="'button'"
+      :buttonDisable="false"
+      :buttonSize="'md'"
+      :buttonOutline="false"
+      :buttonFlat="false"
+      :buttonRounded="false"
+      :buttonSquare="true"
+      :buttonPadding="'sm'"
+      :buttonColor="'primary'"
+      :buttonTextColor="'white'"
+      :buttonDense="false"
+      :buttonRound="false"
     >
     </GenericButton>
   </q-page>
