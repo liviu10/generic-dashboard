b29718b6436a5467944b9f22d1807b2dda85f648
liviuvoica <87973797+liviuvoica@users.noreply.github.com>
2022-12-21 17:35:01 +0200

    Refactor the button component

---
+++ src/components/generic/GenericButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-btn
+    :label="label"
+    :icon="icon"
+    :no-caps="noCaps"
+    :no-wrap="noWrap"
+    :stack="stack"
+    :type="type"
+    :disable="disable"
+    :size="size"
+    :outline="outline"
+    :flat="flat"
+    :rounded="rounded"
+    :square="square"
+    :padding="padding"
+    :color="color"
+    :text-color="textColor"
+    :dense="dense"
+    :round="round"
+    @click="emit('onActionClick', true)"
+  />
+</template>
+
+<script setup lang="ts">
+// Import framework related utilities
+import { defineEmits } from 'vue';
+
+interface ButtonProps {
+  label: string | number;
+  icon?: string;
+  noCaps?: boolean;
+  noWrap?: boolean;
+  stack?: boolean;
+  type: string;
+  disable?: boolean;
+  size?: string;
+  outline?: boolean;
+  flat?: boolean;
+  rounded?: boolean;
+  square?: boolean;
+  padding?: string;
+  color?: string;
+  textColor?: string;
+  dense?: boolean;
+  round?: boolean;
+}
+
+const emit = defineEmits<{
+  (event: 'onActionClick', value: boolean): void;
+}>();
+
+withDefaults(defineProps<ButtonProps>(), {});
+</script>
+
+<style lang="scss" scoped>
+</style>
