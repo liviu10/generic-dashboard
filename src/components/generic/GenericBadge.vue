4ee3d4372e6bc52c9890d05763065a29fb12eca9
liviuvoica <87973797+liviuvoica@users.noreply.github.com>
2022-12-22 11:14:37 +0200

    Configured generic badge

---
+++ src/components/generic/GenericBadge.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-badge :label="label" :rounded="rounded" />
+</template>
+
+<script setup lang="ts">
+interface BadgeProps {
+  floating?: boolean;
+  multiLine?: boolean;
+  label: string | number;
+  align?: string;
+  color?: string;
+  textColor?: string;
+  transparent?: boolean;
+  outline?: boolean;
+  rounded?: boolean;
+}
+
+withDefaults(defineProps<BadgeProps>(), {});
+</script>
+
+<style lang="scss" scoped>
+@import 'src/css/utilities/_rem_convertor.scss';
+.q-badge {
+  border-radius: 0rem;
+  padding: rem-convertor(5px);
+}
+</style>
