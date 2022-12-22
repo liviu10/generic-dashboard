28728b2734e3333e51dde925e8c59f946a463a62
liviuvoica <87973797+liviuvoica@users.noreply.github.com>
2022-12-22 17:26:29 +0200

    Configured a generic icon component

---
+++ src/components/generic/GenericIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-icon :size="size" :name="name" />
+</template>
+
+<script setup lang="ts">
+export interface IconProps {
+  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | undefined;
+  name: string;
+  cssClass?: 'q-icon--square' | 'q-icon--round' | undefined;
+}
+
+withDefaults(defineProps<IconProps>(), {});
+</script>
+
+<style lang="scss" scoped></style>
